8e95af8a640a4c7549662bb7f8b8406574e2ac46
Bagdad Youcef <71285835+YoussBGD@users.noreply.github.com>
2025-02-24 14:17:32 +0100

    Delete LSTM_Chem-master_G directory

--- LSTM_Chem-master_G/src/utils/__init__.py
+++
@@ -1,7 +0,0 @@
-from .molecular_utils import calculate_descriptors, calculate_fingerprints, calculate_similarity
-
-__all__ = [
-    'calculate_descriptors',
-    'calculate_fingerprints',
-    'calculate_similarity'
-]
